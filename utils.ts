7cfbe2f3d9b7ca99bdc9faed66535a527c8fe65e
Nathaniel <ndm0113@gmail.com>
2022-12-24 09:04:48 -0500

    Document getExe/getExeWindows in utils

--- utils.ts
+++ utils.ts
@@ -1,5 +1,11 @@
 import {path} from "./deps.ts";
 
+/**
+ * Internal Windows-specific search logic used by getExe.
+ *
+ * @param exe       The executable to find
+ * @param decoder   A decoder for output text
+ */
 async function getExeWindows(exe: string, decoder: TextDecoder) {
     return await Deno.run({
         cmd: ["powershell", "-c", `(gcm ${exe}).Source`],
@@ -31,6 +37,19 @@ async function getExeWindows(exe: string, decoder: TextDecoder) {
         .catch(() => undefined);
 }
 
+/**
+ * Try resolving the path of an executable by name.  On Unix-like systems this
+ * uses `which`.  On Windows, it will attempt to:
+ * - Use `powershell`'s `gcm` to find the executable in the path
+ * - Use `powershell` to get the Program Files directory and try to find an
+ *   `MKVToolNix` folder, then try to find the executable there with an `.exe`
+ *   extension
+ *
+ * In either case, it will first check for an environment variable that shares
+ * its name with the executable and use that if set.
+ *
+ * @param exe   The executable to find
+ */
 export async function getExe(exe: string): Promise<string> {
     const env = exe.toUpperCase();
     let file = Deno.env.get(env);
@@ -62,6 +81,12 @@ export async function getExe(exe: string): Promise<string> {
     return file;
 }
 
+/**
+ * Shorthand for `() => { a(); b(); }`.
+ *
+ * @param a Void function to reduce
+ * @param b Void function to reduce
+ */
 export function reduceVoid(a: () => void, b: () => void): () => void {
     return () => {
         a();
@@ -69,7 +94,12 @@ export function reduceVoid(a: () => void, b: () => void): () => void {
     };
 }
 
+/**
+ * Shorthand for `Promise.all([a, b]).then(() => {})`.
+ *
+ * @param a Promise<void> to reduce
+ * @param b Promise<void> to reduce
+ */
 export function reduceVoidPromise(a: Promise<void>, b: Promise<void>): Promise<void> {
-    return Promise.all([a, b]).then(() => {
-    });
+    return Promise.all([a, b]).then(() => {});
 }
